38f45d427518aea3f526664e59188c7072a826c6
Busayo <damayor12@yahoo.com>
2023-11-03 22:31:54 -0400

    setup done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// import { server } from './mocks/server';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Home', () => {
+  // beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
+  // afterEach(() => server.resetHandlers());
+  // afterAll(() => server.close());
+
+  test('renders learn react lin', async () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+
+    // await screen.findByText(/john/i);
+    screen.debug();
+    expect(linkElement).toBeInTheDocument();
+
+    expect(await screen.findByText(/john/i)).toBeInTheDocument();
+
+    // await waitFor(() => {
+    //   expect(await screen.findByText(/john/)).toBeInTheDocument();
+    // });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
+import NavBar from './components/NavBar/NavBar';
+import { Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Home from './pages/Home';
+import Load from './pages/Load';
+import { GlobalStyle } from './styles';
+// import { ScheduleProvider } from './hooks/useShifts';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+
+      {/* <ScheduleProvider> */}
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/load">
+          <Load />
+        </Route>
+      </Switch>
+      {/* </ScheduleProvider> */}
+
+      <ToastContainer position="bottom-right" autoClose={5000} />
+    </>
   );
 }
 
